a1fb1ff5c368f9cede6b1404b1d849f0f20cb327
Vivek Sharma <therealviveksharma@gmail.com>
2026-08-07 21:56:45 +0530

    added navbar

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,16 @@ import businessProfileRouter from "./routes/businessProfileRouter.js";
 import aiInvoiceRouter from "./routes/aiInvoiceRouter.js";
 
 const app = express();
+
 const port = process.env.PORT || 4000;
 
 // Middlewares
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
+);
 app.use(clerkMiddleware());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
@@ -21,7 +27,7 @@ app.use(express.urlencoded({ limit: "20mb", extended: true }));
 connectDB();
 
 // Routes
-app.use("uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/api/invoice", invoiceRouter);
 app.use("/api/businessProfile", businessProfileRouter);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <div className='text-xl bg-amber-200'>App</div>
-  )
-}
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
+};
 
-export default App
+export default App;
