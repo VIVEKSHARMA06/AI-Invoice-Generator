5272b4bb04dac00d07df5b4175386105d4d02a58
Vivek Sharma <therealviveksharma@gmail.com>
2026-08-10 01:43:42 +0530

    added ui features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AppShell from "./components/Appshell";
+import Dashboard from "./pages/Dashboard";
+import { Show, RedirectToSignIn } from "@clerk/react";
+
+const ClerkProtected = ({ children }) => (
+    <>
+        <Show when="signed-in">{children}</Show>
+        <Show when="signed-out">
+            <RedirectToSignIn />
+        </Show>
+    </>
+);
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
+        <div className="min-h-screen max-w-full overflow-x-hidden">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* it must be a protected route */}
+                <Route
+                    path="/app"
+                    element={
+                        <ClerkProtected>
+                            <AppShell />
+                        </ClerkProtected>
+                    }
+                >
+                    <Route index element={<Dashboard />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </div>
     );
 };
 
