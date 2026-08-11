9fb0047293560becbc9ba5417e28402457ea75a8
Vivek Sharma <therealviveksharma@gmail.com>
2026-08-12 00:48:17 +0530

    added features component

---
+++ frontend/src/components/Pricing.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { pricingStyles, pricingCardStyles } from "../assets/dummyStyles";
+
+const Pricing = () => {
+    return <div>Pricing</div>;
+};
+
+export default Pricing;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Pricing from "../components/Pricing";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <main className="">
+                <Hero />
+                <div className=" ">
+                    <Features />
+                </div>
+                <Pricing />
+            </main>
         </div>
     );
 };
